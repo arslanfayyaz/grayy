e67c528bb6548134560a50500b69300e8b8036f1
arslanfayyaz <arslanghouri500@gmail.com>
2023-07-24 19:02:52 +0500

    removing comments

--- app1/views.py
+++ app1/views.py
@@ -5,10 +5,11 @@ class CustomList(UserList):
     def __init__(self, iterable):
         super().__init__(self._validate_item(item) for item in iterable)
 
-    def __setitem__(self, index, item):
+    def setitem(self, index, item):
         self.data[index] = self._validate_item(item)
 
     def insert(self, index, item):
+
         if index < 0:
             index += len(self.data) + 1
 
@@ -18,7 +19,7 @@ class CustomList(UserList):
         self.data = self.data[:index] + [self._validate_item(item)] + self.data[index:]
 
     def _validate_and_append(self, item):
-        # Custom validation logic for append
+
         if isinstance(item, (int, float, str)):
             self.data = self.data + [item]
 
@@ -26,7 +27,7 @@ class CustomList(UserList):
             raise TypeError(f"Valid data type expected, got {type(item).__name__}")
 
     def _validate_and_extend(self, other):
-        # Custom validation logic for extend
+
         if isinstance(other, type(self)):
             self.data = self.data + other
 
@@ -40,12 +41,14 @@ class CustomList(UserList):
                     raise TypeError(f"Valid data type expected, got {type(item).__name__}")
 
     def _validate_item(self, value):
+
         if isinstance(value, (int, float, str)):
             return value
 
         raise TypeError(f"Valid data type expected, got {type(value).__name__}")
 
     def pop(self, index=-1):
+
         if not self.data:
             raise IndexError("pop from empty list")
 
@@ -82,6 +85,7 @@ class CustomList(UserList):
         return filtered_list
 
     def for_each(self, func):
+
         for item in self.data:
             func(item)
 
@@ -114,32 +118,27 @@ my_list.extend([5, '6', 7.77])
 print(my_list)
 
 joined_string = my_list.join(", ")
-print(joined_string)  # Output: "1, 2, 3.14, hello, 42"
+print(joined_string)
 
-# Using the map method
 multiplied_list = my_list.map(lambda x: x * 2)
-print(multiplied_list)  # Output: [2, '22', 6.28, 'hellohello', 84]
+print(multiplied_list)
 
-# Using the filter method
 filtered_list = my_list.filter(lambda x: isinstance(x, int))
-print(filtered_list)  # Output: [1, 42]
+print(filtered_list)
 
 copied_list = my_list.copy()
-print(copied_list)  # Output: [1, '2', 3.14, 'hello', 42]
+print(copied_list)
 
-# Using the count method
 count_of_2 = my_list.count('2')
-print(count_of_2)  # Output: 1
+print(count_of_2)
 
 popped_item = my_list.pop()
 print(popped_item)
 print(my_list)
 
-# Using the remove method
 my_list.remove('2')
 print(my_list)
 
-# Using the reverse method
 my_list.reverse()
 print(my_list)
 
